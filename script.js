9b60128f60ea39ba2858ae74e64e5decf5b9dcb3
KaushalChawale <86220366+KaushalChawale@users.noreply.github.com>
2022-02-07 12:37:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
-let countDate = new Date('March 1, 2022 00:00:00').getTime();
+let countDate = new Date('April 1, 2022 00:00:00').getTime();
 
 function countDown(){
 
